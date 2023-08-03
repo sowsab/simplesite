a11ed9f78c436f8d6243f347cb8e6fcfd15ff3de
sowsab <sowsab@naver.com>
2023-08-03 10:14:08 +0900

    sql 일부 변경

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -3,12 +3,15 @@ VALUES (1, 'test@test.com', 'test', '123'),
        (2, 'test2@test.com', 'test2', '123');
 
 INSERT INTO `post` (`idx`, `title`, `content`, `user_idx`, `create_date`, `update_date`, `delete_date`)
-VALUES (1, '안녕하세요', '안녕하지 못해요', 1, '2023-08-01 13:26:43.000', NULL, NULL),
-       (2, '안녕하지못해요', '사실 안녕해요', 1, '2023-08-01 13:27:15.000', NULL, NULL),
-       (3, 'ㅎㅇ', 'ㅎㅇ', 2, '2023-08-01 13:35:26.000', NULL, NULL);
+VALUES (1, '안녕하세요', '안녕하지 못해요', 1, NOW(), NULL, NULL),
+       (2, '안녕하지못해요', '사실 안녕해요', 1, NOW(), NULL, NULL),
+       (3, 'ㅎㅇ', 'ㅎㅇ', 2, NOW(), NULL, NULL);
 
 
 INSERT INTO `comment`
 (`idx`, `content`, `post_idx`, `user_idx`, `create_date`, `update_date`, `delete_date`)
-VALUES (1, 'ㅎㅇ', 1, 2, '2023-08-01 13:37:58.000', NULL, NULL),
-       (2, 'ㅎㅇ', 3, 1, '2023-08-01 13:38:19.000', NULL, NULL);
+VALUES (1, 'ㅎㅇ', 1, 2, NOW(), NULL, NULL),
+       (2, 'ㅎㅇ', 3, 1, NOW(), NULL, NULL),
+       (3, '뉘신지?', 1, 1, NOW(), NULL, NULL),
+       (4, 'ㅎㅇ', 3, 1, NOW(), NULL, NULL),
+       (5, 'ㅎㅇ', 3, 1, NOW(), NULL, NULL);
