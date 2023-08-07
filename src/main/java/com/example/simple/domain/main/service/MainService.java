aa090ee0f0cb41eda079e0961e56aa393d2607b6
sowsab <sowsab@naver.com>
2023-08-07 16:36:19 +0900

    미사용 코드 제거

--- src/main/java/com/example/simple/domain/main/service/MainService.java
+++ src/main/java/com/example/simple/domain/main/service/MainService.java
@@ -1,10 +1,7 @@
 package com.example.simple.domain.main.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 
 import com.example.simple.common.dto.LoginDTO;
